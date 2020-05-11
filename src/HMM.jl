9ed4b45805b56340f5c8af77eb73ee3f4fe78c53
Ethan Mines <elmines325@gmail.com>
2020-05-11 14:30:20 -0500

    Add simple Markov process simulation

--- src/HMM.jl
+++ src/HMM.jl
@@ -1,5 +1,27 @@
 module HMM
 
-greet() = print("Hello World!")
+RowVector{T} = Array{T, 2}
+
+struct Model
+	transition::Matrix{Float64}
+	emission::Matrix{Float64}
+	function Model(trans, emis)
+		if size(trans, 2) != size(emis, 1)
+			throw(ArgumentError("Inner dimensions of transition and emission must match"))
+		end
+		new(trans, emis)
+	end
+end
+
+function simulate(m::Model, p::RowVector{Float64}, n::Int) :: RowVector{Float64}
+	if size(p, 2) != size(m.transition, 1)
+		throw(ArgumentError("p must have same number of states as m"))
+	end
+	accum = p;
+	for i in 1:n
+		accum = accum * m.transition
+	end
+	accum * m.emission
+end
 
 end # module
