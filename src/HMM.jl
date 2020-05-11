25eb4ce2c89cc7ca039d6b014331833afbe76a09
Ethan Mines <elmines325@gmail.com>
2020-05-11 16:52:20 -0500

    Implement forward algorithm

--- src/HMM.jl
+++ src/HMM.jl
@@ -4,24 +4,40 @@ RowVector{T} = Array{T, 2}
 
 struct Model
 	transition::Matrix{Float64}
-	emission::Matrix{Float64}
-	function Model(trans, emis)
+    emission::Matrix{Float64}
+    initial::RowVector{Float64}
+	function Model(trans, emis, init)
 		if size(trans, 2) != size(emis, 1)
 			throw(ArgumentError("Inner dimensions of transition and emission must match"))
-		end
-		new(trans, emis)
+        end
+        if size(init, 2) != size(trans, 1)
+            throw(ArgumentError("Inner dimensions of initial and transtion must match"))
+        end
+		new(trans, emis, init)
 	end
 end
 
-function simulate(m::Model, p::RowVector{Float64}, n::Int) :: RowVector{Float64}
-	if size(p, 2) != size(m.transition, 1)
-		throw(ArgumentError("p must have same number of states as m"))
-	end
-	accum = p;
+function simulate(m::Model, n::Int) :: RowVector{Float64}
+	accum = m.initial;
 	for i in 1:n
 		accum = accum * m.transition
 	end
 	accum * m.emission
 end
+
+function likelihood(m::Model, o::Vector{Int})::Float64
+    T = size(o,1)
+    em = m.emission
+    joint_probs = m.initial .* transpose(em[:,o[1]])
+    println(joint_probs)
+    for i in 2:T
+        joint_probs = joint_probs * m.transition
+        joint_probs = joint_probs .* transpose(em[:,o[i]])
+        println(joint_probs)
+    end
+    sum(joint_probs)
+end
+
+export Model, simulate, likelihood
 
 end # module
