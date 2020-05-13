158a53425b807da848ce9137d0a6ca8c6d9e0d4c
Ethan Mines <elmines325@gmail.com>
2020-05-13 15:04:22 -0500

    WIP: Train HMM from single sample

--- src/HMM.jl
+++ src/HMM.jl
@@ -36,6 +36,9 @@ function likelihood(m::Model, o::Vector{Int})::Float64
     sum(joint_probs)
 end
 
+"""
+Has runtime Θ(TN^2) where T = |o| and N is the number of states
+"""
 function decode(m::Model, o::Vector{Int})::Vector{Int}
     T = size(o, 1)
     N = size(m.transition, 1)
@@ -43,20 +46,52 @@ function decode(m::Model, o::Vector{Int})::Vector{Int}
     trans = m.transition
     em = m.emission
     joint_probs = dropdims( m.initial .* transpose(em[:, o[1]]), dims=1)
-    for t = 2:T
-        (probs, inds) = findmax(joint_probs .* trans, dims=1)
-        probs = dropdims(probs, dims=1) .* em[:,o[t]]
-        backpointers[t, :] = transpose([i[1] for i in inds])
+    for t = 2:T                                                   # Θ(TN^2)
+        (probs, inds) = findmax(joint_probs .* trans, dims=1)       # Θ(N^2)
+        probs = dropdims(probs, dims=1) .* em[:,o[t]]               # Θ(N)
+        backpointers[t, :] = transpose([i[1] for i in inds])        # Θ(N)
         joint_probs = probs
     end
 
     decoded = Vector{Int}(undef, T)
     decoded[T] = findmax(joint_probs)[2]
-    for t = T:-1:2
+    for t = T:-1:2                                                # Θ(T)
         next = backpointers[t, decoded[t]]
         decoded[t-1] = next
     end
     decoded
+end
+
+function learn(O::Vector{Int}, outsize::Int, hiddensize::Int)::Model
+    # Initialize transition matrix to a uniform distribution
+    trans::Array{Float64,2} = ones(hiddensize,hiddensize) / hiddensize
+    emit::Array{Float64,2} = ones(hiddensize, outsize) / outsize
+    T::Int = size(O)
+
+    prog = 1
+    while prog >= 0.1
+        ξ = zeros(hiddensize, hiddensize, T-1)
+        γ = zeros(hiddensize, T)
+
+        trans_new = similar(trans)
+        for i = 1:hiddensize
+            for j = 1:hiddensize
+                trans_new[i,j] = sum(ξ[i,j]) / sum(ξ[i])
+            end
+        end
+
+        emit_new = similar(emit)
+        for k = 1:outsize
+            for j = 1:hiddensize
+
+            end
+        end
+
+
+        prog = 0.01
+    end
+
+    init = ones(1, hiddenSize) / hiddensize #FIXME: Calcluate from α(1)
 end
 
 export Model, simulate, likelihood, decode
